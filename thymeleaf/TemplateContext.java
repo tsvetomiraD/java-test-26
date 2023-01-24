d70712aa92c76e30b099cfdad042c084c10b0333
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2023-01-24 20:18:12 +0200

    Update TemplateContext.java

--- thymeleaf/TemplateContext.java
+++ thymeleaf/TemplateContext.java
@@ -5,12 +5,8 @@ import java.util.List;
 import java.util.Map;
 
 public class TemplateContext {
-    Map<String, String> welcomeMessages = new HashMap<>();
-    Map<String, Object[]> classes = new HashMap<>();
-    public void put(String name, WelcomeMessage welcomeMessage) {
-        welcomeMessages.put(name, welcomeMessage.getMessage());
-    }
-    public void put(String name, Object[] receivedClasses) {
-            classes.put(name, receivedClasses);
+    Map<String, Object> classes = new HashMap<>();
+    public void put(String name, Object receivedClasse) {
+        classes.put(name, receivedClasse);
     }
 }
